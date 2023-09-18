247bdc5b6214bb0a14a7f32d7212cdece500e477
Thaddeus Cleo <thaddeuscleo@gmail.com>
2023-09-19 00:57:28 +0700

    feat: add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,31 @@
+<h1>The man behind the Jacket</h1>
+<br />
+
+<p>
+	Hey there, I'm your friendly neighborhood twenty-something explorer, rocking the role of a DevOps
+	Engineer extraordinaire in the mysterious realm of ID.
+</p>
+<br />
+
+<p>
+	Before I embarked on this epic DevOps journey, I had my fair share of adventures as the Network
+	Ninja at the University Software Laboratory. 🚀💻🌟
+</p>
+<br />
+
+<h2>The Gears?</h2>
+<p>
+	Uh-oh, I'm in deep trouble here! 🙈 I've got a collection of these things, and they all seem to
+	have a mind of their own! I swear, they're all different and unique, each with its own special
+	purpose... or so I tell myself!
+</p>
+<br>
+
+<h2>The Computers?</h2>
+<br />
+
+<p>
+	Oh, they're my digital buddies, my silicon soulmates! 🖥️🤖 I've got this thing for Linux and Open
+	Source that's as strong as a titanium-plated keyboard
+</p>
+<br />
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,1 @@
+A view on the tech and open source world from top of the mountain
